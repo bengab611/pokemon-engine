7425ad19a8a5210157f543af1f4036b8cef2dab6
bengab611 <bengab611@gmail.com>
2026-03-10 19:06:21 -0400

    Added Trainer class

---
+++ src/pokemon/Trainer.java
@@ -0,0 +1,29 @@
+package pokemon;
+
+public class Trainer {
+    String name;
+    Pokemon[] team;
+
+    public Trainer(String name) {
+        this.name = name;
+        team = new Pokemon[6];
+    }
+
+    public void addPokemon(Pokemon newPkmn) {
+        for (Pokemon pkmn : team) {
+            if (pkmn == null) {
+                pkmn = newPkmn;
+                return;
+            }
+        }
+
+        System.out.println("Team already has six Pokemon.");
+    }
+
+    public void replacePokemon(Pokemon newPkmn, int pkmnToReplace) {
+        team[pkmnToReplace] = newPkmn;
+    }
+
+    public Pokemon getPokemon(int index) { return team[index]; }
+    public Pokemon[] getTeam() { return team; }
+}
